2e643052f3077a34841bf11a999ac7af52242b27
jstevens79 <jstevens79@users.noreply.github.com>
2019-04-01 18:40:52 -0500

    setting the default song for spotify

--- spotify.js
+++ spotify.js
@@ -4,13 +4,25 @@ var keys = require('./keys.js');
 var spotify = new Spotify(keys.spotify)
 
 var getSpotify = function(a, def) {
+
+  if (!def) {
+    console.log('\nSearching for song...\n');
+  }
+
   spotify.search({
     type: 'track',
     query: a,
   }).then(function(resp) {
 
     if (resp.tracks.items.length) {
-      resp.tracks.items.forEach(function(item) {
+      var songList = resp.tracks.items;
+
+      if (def) {
+        songList = resp.tracks.items.filter(item => item.album.artists[0].name === 'Ace of Base');
+        songList = songList.filter(song => song.name === 'The Sign');
+      }
+      
+      songList.forEach(function(item) {
         var artist = item.album.artists[0].name;
         var songName = item.name;
         var previewLink = item.preview_url;
@@ -19,10 +31,13 @@ var getSpotify = function(a, def) {
         console.log('Song Title: ', songName);
         console.log('Preview Link: ', previewLink);
         console.log('Album: ', album);
-        console.log('-----------------------\n');
+        console.log('\n-----------------------\n');
       })
+
+      
     } else {
       // if no results, default to "the sign" by ace of base
+      console.log('\nNo results found. How about a classic song?\n');
       getSpotify("the sign", true);
     }
 
